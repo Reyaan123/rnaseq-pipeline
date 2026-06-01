324fb25faae9e28d845c613a539733182c52667f
Reyaan <reyaan6102@gmail.com>
2026-06-01 15:45:48 -0400

    refactor: neatness

--- fastq_parser.py
+++ fastq_parser.py
@@ -1,16 +1,18 @@
 import sys
 filename = sys.argv[1]
+
+i=0
+sequences=[]
+
 with open(filename, "r") as f:
  
-    i=0
-    sequences=[]
-
+   
     for line in f:
-        i=i+1
+        i+=1
         if i%4==2:
             sequences.append(line.strip())
 
-    print(f"Total Lines: {i} \nTotal reads: {i//4} ")
-    print("Sequences:")
-    for seq in sequences:
-         print(seq)
+print(f"Total Lines: {i} \nTotal reads: {i//4} ")
+print("Sequences:")
+for seq in sequences:
+     print(seq)
