20477af24ddd3554fee5483c22178ca0f0325eb3
Reyaan <reyaan6102@gmail.com>
2026-06-01 15:52:33 -0400

    feat: add len of string in print

--- fastq_parser.py
+++ fastq_parser.py
@@ -3,7 +3,6 @@ filename = sys.argv[1]
 
 i=0
 sequences=[]
-
 with open(filename, "r") as f:
  
    
@@ -11,8 +10,8 @@ with open(filename, "r") as f:
         i+=1
         if i%4==2:
             sequences.append(line.strip())
-
 print(f"Total Lines: {i} \nTotal reads: {i//4} ")
 print("Sequences:")
 for seq in sequences:
-     print(seq)
+     print(seq , len(seq) ,"bp")
+    
