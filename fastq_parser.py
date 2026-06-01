0c1bcf1ae7d5f95a154295082402728221afc88e
Reyaan <reyaan6102@gmail.com>
2026-06-01 15:28:27 -0400

    feat: extract and disoplay fastq dna sequences

--- fastq_parser.py
+++ fastq_parser.py
@@ -3,7 +3,12 @@ filename = sys.argv[1]
 with open(filename, "r") as f:
  
     i=0
+    sequences=[]
     for line in f:
         i=i+1
+        if i%4==2:
+            sequences.append(line.strip())
 
     print(f"Total Lines: {i} \nTotal reads: {i//4} ")
+    for seq in sequences:
+         print(seq)
