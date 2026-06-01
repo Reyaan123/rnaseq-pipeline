b03e2acbb3657dbaea0bb92b57e6bb22a12ad97d
Reyaan <reyaan6102@gmail.com>
2026-06-01 15:38:16 -0400

    refactor: clean up var

--- fastq_parser.py
+++ fastq_parser.py
@@ -4,11 +4,13 @@ with open(filename, "r") as f:
  
     i=0
     sequences=[]
+
     for line in f:
         i=i+1
         if i%4==2:
             sequences.append(line.strip())
 
     print(f"Total Lines: {i} \nTotal reads: {i//4} ")
+    print("Sequences:")
     for seq in sequences:
          print(seq)
