c96465e9b8b75990a29dd64741a8994ecf03f710
Reyaan <reyaan6102@gmail.com>
2026-07-14 15:56:28 -0700

    feat: add bar chart to Streamlit dashboard

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,12 @@ import pandas as pd
 conn = sqlite3.connect("pipeline_results.db")
 cursor = conn.cursor()
 
-st.title("RNA-Seq Pipleine Results")
 
 data = cursor.execute("SELECT * FROM expression_results")
 results = cursor.fetchall()
+df= pd.DataFrame(results, columns=["gene_name","mean_healthy","mean_cancer", "p_value"])
+
+st.title("RNA-Seq Pipleine Results")
+st.subheader("Gene expression comparason")
+st.bar_chart(df.set_index("gene_name")[["mean_healthy", "mean_cancer"]])
 st.dataframe(results)
